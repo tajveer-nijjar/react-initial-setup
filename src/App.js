339c40dc8fe06c850b45dbb75e23754776d90e31
Taj <taj.nijjar2002@gmail.com>
2020-06-17 11:50:06 +1200

    Added sample route and connecting with react-router-dom success.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import HomePage from './containers/HomePage';
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -15,6 +18,11 @@ function App() {
     <React.Fragment>
       <CssBaseline />
       <p className={classes.heading}>Welcome to Haztrak.</p>
+      <Switch>
+        <Route path="/home" component={HomePage} />
+
+        <Redirect from="/" to="/home" />
+      </Switch>
     </React.Fragment>
   );
 }
