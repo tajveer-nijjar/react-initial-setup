81b4213034edbf4903def41b570094690d686d6f
Taj <taj.nijjar2002@gmail.com>
2020-06-17 10:52:38 +1200

    Removed unneccessary files.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store';
