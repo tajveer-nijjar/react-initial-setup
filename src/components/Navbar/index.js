6e9382735ef643e8286336b74550ba7ced28d48f
Taj <taj.nijjar2002@gmail.com>
2020-06-18 09:27:57 +1200

    Added Navbar.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,17 +2,49 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import { useAuth0 } from '../../react-auth0-spa';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const classes = useStyles();
 
   return (
     <div>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in</button>
-      )}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+
+          {!isAuthenticated && (
+            <Button color="inherit" onClick={() => loginWithRedirect({})}>
+              Log in
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
       {isAuthenticated && (
--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ ReactDOM.render(
           <Auth0Provider
             domain={config.domain}
             client_id={config.clientId}
-            redirect_uri={window.location.origin}
+            redirect_uri={redirectUrl}
             onRedirectCallback={onRedirectCallback}
           >
             <App />
